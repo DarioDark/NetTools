40dda425443b9fcfb98a6e16076220eec323f1d6
Darmogus <76487117+DarioDark@users.noreply.github.com>
2024-06-26 17:26:36 +0200

    Update subnetCalculator.py

--- Windows/subnetCalculator.py
+++ Windows/subnetCalculator.py
@@ -1,12 +1,13 @@
 import sys
 
-def calc_subnet(cidr: int) -> str:
+def subnetCalculator(cidr: int) -> str:
     """Calculate the subnet mask for a given CIDR value."""
     if not 0 <= cidr <= 32:
         raise ValueError("CIDR value must be between 0 and 32")
     binary_string: str = "1" * cidr + "0" * (32 - cidr) # Create a binary string of 1s and 0s
-    sub_net: str = ".".join([str(int(binary_string[i:i+8], 2)) for i in range(0, 32, 8)]) # Convert the binary string to a list of 4 octets and join them with a dot
-    return f"Subnet : {sub_net}\nNumber of hosts : {2**(32-cidr)-2}"
+    byte_list :list[str] = [binary_string[i:i+8] for i in range(0, 32, 8)] # Convert the binary string to a list of 4 octets
+    subnet: str = ".".join(list(str(int(byte, 2)) for byte in byte_list)) # Convert the binary numbers to decimal numbers and join them with a dot
+    return f"Subnet : {subnet}\nNumber of hosts : {2**(32-cidr)-2}"
 
 
 if __name__ == "__main__":
